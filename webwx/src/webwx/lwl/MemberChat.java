2bacb7ef099099093b8369719d1499fb37fa1f85
lanwl <lanvd@163.com>
2016-09-02 18:04:00 +0800

    gg

---
+++ webwx/src/webwx/lwl/MemberChat.java
@@ -0,0 +1,12 @@
+package webwx.lwl;
+
+import java.util.ArrayList;
+
+public class MemberChat {
+	public String UserName = "";
+	public String NickName = "";
+	public ArrayList<String> MemberList;
+   public MemberChat() {
+	   MemberList = new ArrayList<String>();
+   }
+}
